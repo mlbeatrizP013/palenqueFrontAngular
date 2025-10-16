1bfaffd14c501c1631acaa922e8ab36cf35d3070
Saul Luis <saulluislopes45418@gmail.com>
2025-10-16 13:29:02 -0600

    creacion de services de foro y de tienda

---
+++ src/app/modules/tienda/gestion-tienda/gestion-tienda.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-tienda',
+  imports: [],
+  templateUrl: './gestion-tienda.html',
+  styleUrl: './gestion-tienda.css'
+})
+export class GestionTienda {
+
+}

94fd8b5a2a6e7761eea2691bf4a2576eac66167a
Saul Luis <saulluislopes45418@gmail.com>
2025-10-16 15:35:00 -0600

    agregar ruta de gestion de tienda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { GestionTienda } from './modules/tienda/gestion-tienda/gestion-tienda';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '**',
+    pathMatch: 'full' 
+  },
+  {
+    path: 'gestion-tienda',
+    component: GestionTienda
+  }
+];

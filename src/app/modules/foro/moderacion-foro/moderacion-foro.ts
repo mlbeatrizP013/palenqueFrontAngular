2669276d09faa879f487a7daa3dc752edeab263c
Saul Luis <saulluislopes45418@gmail.com>
2025-10-16 13:42:54 -0600

    componentes de foro

---
+++ src/app/modules/foro/moderacion-foro/moderacion-foro.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moderacion-foro',
+  imports: [],
+  templateUrl: './moderacion-foro.html',
+  styleUrl: './moderacion-foro.css'
+})
+export class ModeracionForo {
+
+}

1d4f6eb7237f2b3b52ab10302ac92512b50fb35b
Saul Luis <saulluislopes45418@gmail.com>
2025-10-17 05:43:00 -0600

    rutas de los compoentes de tienda y foro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { GestionTienda } from './modules/tienda/gestion-tienda/gestion-tienda';
+import { Catalogo } from './modules/tienda/catalogo/catalogo';
+import { AgregarEditarProducto } from './modules/tienda/agregar-editar-producto/agregar-editar-producto';
+import { ModeracionForo } from './modules/foro/moderacion-foro/moderacion-foro';
 
 export const routes: Routes = [
   {
@@ -10,5 +13,17 @@ export const routes: Routes = [
   {
     path: 'gestion-tienda',
     component: GestionTienda
+  },
+  {
+    path: 'catalogo',
+    component: Catalogo
+  },
+  {
+    path: 'agregar-editar-producto',
+    component: AgregarEditarProducto
+  },
+  {
+    path: 'moderacion-foro',
+    component: ModeracionForo
   }
 ];

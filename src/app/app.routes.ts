eea54d037ddcc2ce76a9605f52cae1c33c43972f
Saul Luis <saulluislopes45418@gmail.com>
2025-11-17 13:58:55 -0600

    borrar carpetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,8 @@
 import { Routes } from '@angular/router';
-import { GestionTienda } from './modules/tienda/gestion-tienda/gestion-tienda';
-import { Catalogo } from './modules/tienda/catalogo/catalogo';
-import { AgregarEditarProducto } from './modules/tienda/agregar-editar-producto/agregar-editar-producto';
-import { ModeracionForo } from './modules/foro/moderacion-foro/moderacion-foro';
-
 export const routes: Routes = [
   {
     path: '',
     redirectTo: '**',
     pathMatch: 'full' 
   },
-  {
-    path: 'gestion-tienda',
-    component: GestionTienda
-  },
-  {
-    path: 'catalogo',
-    component: Catalogo
-  },
-  {
-    path: 'agregar-editar-producto',
-    component: AgregarEditarProducto
-  },
-  {
-    path: 'moderacion-foro',
-    component: ModeracionForo
-  }
 ];

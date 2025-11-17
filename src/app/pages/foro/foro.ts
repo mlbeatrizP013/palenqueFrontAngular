7361c0691c300a11d4de8d83738de2935052eb54
Saul Luis <saulluislopes45418@gmail.com>
2025-11-17 14:50:11 -0600

    creacio  de componentes

---
+++ src/app/pages/foro/foro.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foro',
+  imports: [],
+  templateUrl: './foro.html',
+  styleUrl: './foro.css'
+})
+export class Foro {
+
+}
---
+++ src/app/pages/solicitudes/solicitudes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitudes',
+  imports: [],
+  templateUrl: './solicitudes.html',
+  styleUrl: './solicitudes.css'
+})
+export class Solicitudes {
+
+}

c00996c6269f9317fbbf9ef022774a5211bb4021
Ale <alex1313shi@gmail.com>
2025-11-17 07:58:07 -0600

    quitar elementos que no se utilizaran

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
-import { GestionTienda } from './modules/tienda/gestion-tienda/gestion-tienda';
-import { Catalogo } from './modules/tienda/catalogo/catalogo';
-import { AgregarEditarProducto } from './modules/tienda/agregar-editar-producto/agregar-editar-producto';
-import { ModeracionForo } from './modules/foro/moderacion-foro/moderacion-foro';
+
 
 export const routes: Routes = [
   {
@@ -10,20 +7,5 @@ export const routes: Routes = [
     redirectTo: '**',
     pathMatch: 'full' 
   },
-  {
-    path: 'gestion-tienda',
-    component: GestionTienda
-  },
-  {
-    path: 'catalogo',
-    component: Catalogo
-  },
-  {
-    path: 'agregar-editar-producto',
-    component: AgregarEditarProducto
-  },
-  {
-    path: 'moderacion-foro',
-    component: ModeracionForo
-  }
+  
 ];

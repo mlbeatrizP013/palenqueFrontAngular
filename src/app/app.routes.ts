436db434f3885c6b5f6363fb32d58fc803c26252
Juan Antonio <11.juanantonio.102@gmail.com>
2025-11-21 11:44:04 -0600

    ForoPalenque

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ForoComponent } from './foro/foro.component';
 import { TiendaComponent } from './tienda/tienda.component';
 import { SolicitudComponent } from './solicitud/solicitud.component';
+import { ForoDetalleComponent } from './foro-detalle/foro-detalle.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'tabs', pathMatch: 'full' },
@@ -15,6 +16,8 @@ export const routes: Routes = [
       { path: 'foro', component: ForoComponent },
       { path: 'tienda', component: TiendaComponent },
       { path: 'solicitudes',component: SolicitudComponent },
+      { path: 'foro',component: ForoComponent },
+      { path: 'foro/:id',component: ForoDetalleComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
     ],
   },
--- src/app/foro/foro.component.ts
+++ src/app/foro/foro.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <--- IMPORTANTE
+import { BloggerService } from '../services/blogger.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-foro',
   templateUrl: './foro.component.html',
   styleUrls: ['./foro.component.scss'],
   standalone: true,
+  imports: [CommonModule], // <--- IMPORTANTE: Aquí vive el 'date' pipe
 })
-export class ForoComponent  implements OnInit {
+export class ForoComponent implements OnInit {
 
-  constructor() { }
+  posts: any[] = [];
+  loading = true;
 
-  ngOnInit() {}
+  constructor(
+    private bloggerService: BloggerService,
+    private router: Router,
+  ) { }
 
+  ngOnInit() {
+    this.loadPosts();
+  }
+
+  loadPosts(){
+    this.bloggerService.getPosts().subscribe((data: any) => {
+      this.posts = data.items || [];
+      this.loading = false;
+    });
+  }
+
+  openPost(id: string){
+    this.router.navigate(['/foro', id]);
+  }
 }

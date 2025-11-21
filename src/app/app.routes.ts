2e395b145952dbe1ec64cce8ba39d0392e68a24a
Saul Luis <saulluislopes45418@gmail.com>
2025-11-20 21:50:50 -0600

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,53 @@ import { HomeComponent } from './home/home.component';
 import { ForoComponent } from './foro/foro.component';
 import { TiendaComponent } from './tienda/tienda.component';
 import { SolicitudComponent } from './solicitud/solicitud.component';
+// IMPORTANTE: Importa aquí tus componentes de Experiencias y Visita si ya los creaste
+// import { ExperienciasComponent } from './experiencias/experiencias.component';
+// import { VisitaComponent } from './visita/visita.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  },
   {
     path: 'tabs',
-    component: TabsComponent,
+    component: TabsComponent, // <--- CORRECCIÓN CLAVE: Este es el layout (Sidebar + Outlet)
     children: [
-      { path: 'home', component: HomeComponent },
-      { path: 'foro', component: ForoComponent },
-      { path: 'tienda', component: TiendaComponent },
-      { path: 'solicitudes',component: SolicitudComponent },
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-    ],
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'foro',
+        component: ForoComponent
+      },
+      {
+        path: 'tienda', // Esto se usará para el enlace "Catálogo"
+        component: TiendaComponent
+      },
+      {
+        path: 'solicitudes',
+        component: SolicitudComponent
+      },
+      {
+        path: 'experiencias',
+        component: HomeComponent // <--- CAMBIAR por ExperienciasComponent cuando lo tengas
+      },
+      {
+        path: 'visita-personalizada',
+        component: SolicitudComponent // <--- CAMBIAR por VisitaComponent cuando lo tengas
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      }
+    ]
   },
-  { path: '**', redirectTo: 'tabs' },
+  {
+    path: '**',
+    redirectTo: '/tabs/home'
+  }
 ];
